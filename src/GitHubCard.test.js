de2e307b012f35cc5298839d0e133dc7532ca946
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-21 18:57:11 -0700

    created a snapshot test for GitHubCard component

---
+++ src/GitHubCard.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import GitHubCard from './GitHubCard';
+import renderer from 'react-test-renderer'
+
+test('renders a snapshot of GitHubCard', () => {
+    const tree = renderer.create(<GitHubCard />).toJSON()
+    expect(tree).toMatchSnapshot()
+})
